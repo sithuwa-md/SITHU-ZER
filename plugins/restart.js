1062d72fd16c9191fd8f55eb425f7440c43d792e
sithuwa-md <sithumsathsara253@gmail.com>
2025-01-09 14:34:46 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -4,7 +4,7 @@ const {sleep} = require('../lib/functions')
 
 cmd({
     pattern: "restart",
-    desc: "restart the bot",
+    desc: "restarting Bot ⚙️✅",
     category: "owner",
     filename: __filename
 },
